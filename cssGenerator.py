8be05bf3529c0b159b3522343663e4f211ced64e
Cary Riina <cary.e.riina@gmail.com>
2018-02-16 20:56:42 -0500

    lineparts[0] refactoring complete and editted the single quote regex, because it does not need to avoid styling html, since html is in double quotes

--- cssGenerator.py
+++ cssGenerator.py
@@ -7,36 +7,40 @@ def generateCSS(filename):
 		'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if',
 		'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return',
 		'try', 'while', 'with', 'yield',]
-	operations = [' = ', ' \+ ', ' - ', ' [*]+? ', ' / ', ' % ', ' // ',]
+	operations = [' = ', ' \+ ', ' - ', ' [*]+? ', ' / ', ' % ', ' // ', ' > ', ' < ',]
 	output = ''
 	with open(filename, 'r') as f:
 		for line in f:
-			# matching class keyword is special because that appears in added html tags, must go first
-			line = re.sub(r'\bclass\b', '<span class="keyword">class</span>', line)
+			# Split at the hashtags to block comments parts from being syntax highlighted
+			# lineparts[0] is the part before the # after splitting
+			lineparts = line.split('#')
+			# matching class keyword must go first
+			lineparts[0] = re.sub(r'\bclass\b', '<span class="keyword">class</span>', lineparts[0])
+			
 			# add spans around strings
-			if '#' not in line:
-				line = re.sub(r'("[^keyword].*?")', r'<span class="string">\1</span>', line)
-				line = re.sub(r'(\'[^keyword].*?\')', r'<span class="string">\1</span>', line)
+			lineparts[0] = re.sub(r'("[^keyword].*?")', r'<span class="string">\1</span>', lineparts[0])
+			lineparts[0] = re.sub(r'(\'.*?\')', r'<span class="string">\1</span>', lineparts[0])
+			
 			# add spans around dataTypes
 			for dataType in dataTypes:
-				if '#' not in line:
-					line = re.sub(r'\b({})\b'.format(dataType), r'<span class="dataType">\1</span>', line)
+				lineparts[0] = re.sub(r'\b({})\b'.format(dataType), r'<span class="dataType">\1</span>', lineparts[0])
+			
 			# add spans around keywords
 			for keyword in keywords:
-				if '#' not in line:
-					line = re.sub(r'\b({})\b'.format(keyword), r'<span class="keyword">\1</span>', line)
+				lineparts[0] = re.sub(r'\b({})\b'.format(keyword), r'<span class="keyword">\1</span>', lineparts[0])
+			
 			# add spans around operations
 			for operation in operations:
-				lineparts = line.split('#')
 				lineparts[0] = re.sub(r'({})'.format(operation), r'<span class="keyword">\1</span>', lineparts[0])
-				line = '#'.join(lineparts)
+
 			# add spans around functions
-			# if '#' not in line:
-			line = re.sub(r'(\b[^#>].\w+)\(', r'<span class="function">\1</span>(', line)
+			lineparts[0]= re.sub(r'(\b[^#>].\w+)\(', r'<span class="function">\1</span>(', lineparts[0])
 			# # add spans around functionKeywords
 			# if '#' not in line:
 			# 	line = re.sub(r'\b(\w+)=', r'<span class="functionKeywords">\1</span>=', line)
+			
 			# add spans around comments
+			line = '#'.join(lineparts)
 			line = re.sub(r'(\#.*$)', r'<span class="comment">\1</span>', line)
 			# replace tabs with &nbsp;
 			line = re.sub(r'\t', '&nbsp;'*4, line)
@@ -48,5 +52,5 @@ def generateCSS(filename):
 		return output
 
 if __name__ == "__main__":
-	generatedCSS = generateCSS('exampleInput.py')
+	generatedCSS = generateCSS('bigExample.py')
 	print(generatedCSS)
